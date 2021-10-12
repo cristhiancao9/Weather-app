aa9a91f7830d7a98289c86ea15155de3579d9595
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-10-12 10:47:11 -0500

    card style modification

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "../components/Card.css";
-export default function Card(props) {
+export default function Card() {
   return (
     <div className="card">
       <div id="closeIcon" className="row">
-        <button>X</button>
+        <button className="btn btn-sm btn-danger">X</button>
       </div>
-      <div>
+      <div className="card-body">
         <h5 className="card-title">Bogotá</h5>
-        <div>
-          <div>
+        <div className="row">
+          <div className="col-sm-4 col-md-4 col-lg-4">
             <p>Min</p>
             <p>12°</p>
           </div>
-          <div>
+          <div className="col-sm-4 col-md-4 col-lg-4">
             <p>Max</p>
             <p>27°</p>
           </div>
-          <div>
+          <div className="col-sm-4 col-md-4 col-lg-4">
             <img src="" width="80" height="80" alt="" />
           </div>
         </div>
