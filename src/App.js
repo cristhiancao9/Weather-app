86dce527d8cfea20d9922dc4c3ccf57936f0ea3e
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-10-12 11:03:05 -0500

    add object to render multiple cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Nav from "./components/Nav.jsx";
 import "./App.css";
 import Card from "./components/Card.jsx";
-
+import data from "./data.js";
 function App() {
   return (
     <div className="App">
