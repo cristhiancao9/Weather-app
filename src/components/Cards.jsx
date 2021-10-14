274df21326acd44445f7881b12260266dc55385a
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-10-14 12:44:33 -0500

    TimeZone fix

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -15,6 +15,7 @@ export default function Cards({ cities, onClose }) {
             weather={elemento.weather}
             wind={elemento.wind}
             temp={elemento.temp}
+            timeZone={elemento.timeZone}
             onClose={() => onClose(elemento.id)}
           />
         ))}
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -18,7 +18,43 @@ class Clock extends React.Component {
     });
   }
   render() {
-    return <p className="App-clock">{this.state.time}.</p>;
+    return <p className="App-clock">{this.state.time}</p>;
   }
 }
 export default Clock;
+ */
+import React, { Component } from "react";
+
+export default class Clock extends Component {
+  state = {
+    date: new Date(new Date().getTime() + this.props.timeZone * 1000)
+      .toUTCString()
+      .replace(/ GMT$/, ""),
+  };
+
+  timeRefresh() {
+    this.setState({
+      date: new Date(new Date().getTime() + this.props.timeZone * 1000)
+        .toUTCString()
+        .replace(/ GMT$/, ""),
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.timeRefresh();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.date}</span>
+      </div>
+    );
+  }
+}
